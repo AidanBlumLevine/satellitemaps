fe1e9cfc713360a2c8e5482153faea14e01a9b21
aidanzev <aidanblumlevine@gmail.com>
2020-08-14 23:53:47 -0400

    Fixed some bugs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     start();
 });
 
-async function start(){
+async function start() {
     var lat = 40.7182;
     var lon = -74.0060;
     n = Math.pow(2, zoom);
@@ -13,22 +13,220 @@ async function start(){
     y = Math.floor((1 - Math.log(Math.tan(lat * Math.PI / 180) + 1 / Math.cos(lat * Math.PI / 180)) / Math.PI) / 2 * n);
 
     m_ctx = $('.map')[0].getContext('2d');
-    //n_ctx = $('.n_map')[0].getContext('2d');
+    n_ctx = $('.n_map')[0].getContext('2d');
 
     model = await tf.loadLayersModel('model/model.json');
-
+    console.log(getEncodedStyles([
+        {
+            "featureType": "all",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#e5e5e5"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.attraction",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.business",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.government",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.medical",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.place_of_worship",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.school",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.sports_complex",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        }
+    ]))
     createMap();
+}
+
+function getEncodedStyles(styles) {
+    var ret = "";
+    var styleparse_types = { "all": "0", "administrative": "1", "administrative.country": "17", "administrative.land_parcel": "21", "administrative.locality": "19", "administrative.neighborhood": "20", "administrative.province": "18", "landscape": "5", "landscape.man_made": "81", "landscape.natural": "82", "poi": "2", "poi.attraction": "37", "poi.business": "33", "poi.government": "34", "poi.medical": "36", "poi.park": "40", "poi.place_of_worship": "38", "poi.school": "35", "poi.sports_complex": "39", "road": "3", "road.arterial": "50", "road.highway": "49", "road.local": "51", "transit": "4", "transit.line": "65", "transit.station": "66", "water": "6" };
+    var styleparse_elements = { "all": "a", "geometry": "g", "geometry.fill": "g.f", "geometry.stroke": "g.s", "labels": "l", "labels.icon": "l.i", "labels.text": "l.t", "labels.text.fill": "l.t.f", "labels.text.stroke": "l.t.s" };
+    var styleparse_stylers = { "color": "p.c", "gamma": "p.g", "hue": "p.h", "invert_lightness": "p.il", "lightness": "p.l", "saturation": "p.s", "visibility": "p.v", "weight": "p.w" };
+    for (i = 0; i < styles.length; i++) {
+        if (styles[i].featureType) {
+            ret += "s.t:" + styleparse_types[styles[i].featureType] + "|";
+        }
+        if (styles[i].elementType) {
+            if (!styleparse_elements[styles[i].elementType])
+                console.log("style element transcription unkown:" + styles[i].elementType);
+            ret += "s.e:" + styleparse_elements[styles[i].elementType] + "|";
+        }
+        if (styles[i].stylers) {
+            for (u = 0; u < styles[i].stylers.length; u++) {
+                var keys = [];
+                var cstyler = styles[i].stylers[u]
+                for (var k in cstyler) {
+                    if (k == "color") {
+                        if (cstyler[k].length == 7)
+                            cstyler[k] = "#ff" + cstyler[k].slice(1);
+                        else if (cstyler[k].length != 9)
+                            console.log("malformed color:" + cstyler[k]);
+                    }
+                    ret += styleparse_stylers[k] + ":" + cstyler[k] + "|";
+                }
+            }
+        }
+        ret = ret.slice(0, ret.length - 1);
+        ret += ","
+    }
+    return encodeURIComponent(ret.slice(0, ret.length - 1));
 }
 
 function createMap() {
     for (var yTile = 0; yTile < 4; yTile++) {
         for (var xTile = 0; xTile < 4; xTile++) {
-            fetchSatTile(x + xTile, y + yTile).then(cached => {
-                m_ctx.drawImage(cached.img, (cached.x - x) * 256, (cached.y - y) * 256);
+            // fetchSatTile(x + xTile, y + yTile).then(sat => {
+            //     m_ctx.drawImage(sat.img, (sat.x - x) * 256, (sat.y - y) * 256);
+            //     fetchNetTile(x + xTile, y + yTile, sat).then(net => {
+            //         n_ctx.drawImage(net.img, (net.x - x) * 256, (net.y - y) * 256);
+            //     });
+            // });
+            fetchTile(x + xTile, y + yTile);
+        }
+    }
+}
+
+function fetchTile(xTile, yTile) {
+    var matches = sat_cache.filter(c => c.x == xTile && c.y == yTile);
+    if (matches.length > 0) {
+        m_ctx.drawImage(matches[0].img, (matches[0].x - x) * 256, (matches[0].y - y) * 256);
+        var matches2 = net_cache.filter(c => c.x == xTile && c.y == yTile);
+        n_ctx.drawImage(matches2[0].img, (matches2[0].x - x) * 256, (matches2[0].y - y) * 256);
+    } else {
+        var img = new Image();
+        img.crossOrigin = "Anonymous"
+        img.src = 'http://mt0.google.com/vt/lyrs=s&x=' + xTile + '&y=' + yTile + '&z=' + zoom;
+        img.onload = function () {
+            var c = { img: img, x: xTile, y: yTile }
+            sat_cache.push(c);
+            m_ctx.drawImage(c.img, (c.x - x) * 256, (c.y - y) * 256);
+
+            let tensor = tf.browser.fromPixels(c.img).expandDims(0);
+            var out = model.predict(tensor.add(tf.scalar(-127.5)).div(tf.scalar(127.5)));
+            out = out.squeeze().add(tf.scalar(1)).div(tf.scalar(2));
+            var canvas = document.createElement('canvas');
+            canvas.width = 256;
+            canvas.height = 256;
+            tf.browser.toPixels(out, canvas).then(_ => {
+                var c = { img: canvas, x: xTile, y: yTile }
+                net_cache.push(c);
+                n_ctx.drawImage(c.img, (c.x - x) * 256, (c.y - y) * 256);
             });
-            fetchNetTile(x + xTile, y + yTile).then(cached => {
-                //n_ctx.drawImage(cached.img, (cached.x - x) * 256, (cached.y - y) * 256);
-            });
+
+            // $('body')[0].appendChild(canvas);
+            setTimeout(function () {
+
+            }, 500);
         }
     }
 }
@@ -51,36 +249,23 @@ function fetchSatTile(xTile, yTile) {
     });
 }
 
-function fetchNetTile(xTile, yTile) {
+function fetchNetTile(xTile, yTile, sat) {
     return new Promise((resolve, reject) => {
         var matches = net_cache.filter(c => c.x == xTile && c.y == yTile);
         if (matches.length > 0) {
             resolve(matches[0]);
         } else {
-            fetchSatTile(xTile, yTile).then(cached => {
-                let tensor = tf.browser.fromPixels(cached.img).expandDims(0);
-                tensor = tensor.add(tf.scalar(30).sub(tf.mean(tensor)));
-                var out = model.predict(tensor.add(tf.scalar(-127.5)).div(tf.scalar(127.5)));
-                out = out.squeeze().add(tf.scalar(1)).div(tf.scalar(2));
-
-                var canvas = document.createElement('canvas');
-                canvas.width = 256;
-                canvas.height = 256;
-                tf.browser.toPixels(out, canvas);
-                // var img = new Image();
-                // img.src = canvas.toDataURL("image/png");
-                // console.log(img.src);
-                // img.onload = function () {
-                //     var c = { img: img, x: xTile, y: yTile }
-                //     net_cache.push(c);
-                //     // URL.revokeObjectURL(img.src);
-                //     // canvas.remove();
-                //     resolve(c);
-                // }
-                resolve(null);
-                $('body')[0].appendChild(canvas);
-
-            });
+            let tensor = tf.browser.fromPixels(sat.img).expandDims(0);
+            var out = model.predict(tensor.add(tf.scalar(-127.5)).div(tf.scalar(127.5)));
+            out = out.squeeze().add(tf.scalar(1)).div(tf.scalar(2));
+            var canvas = document.createElement('canvas');
+            canvas.width = 256;
+            canvas.height = 256;
+            tf.browser.toPixels(out, canvas);
+            var c = { img: canvas, x: xTile, y: yTile }
+            net_cache.push(c);
+            resolve(c);
+            //$('body')[0].appendChild(canvas);
         }
     });
 }
